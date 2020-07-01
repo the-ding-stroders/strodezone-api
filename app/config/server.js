f85af6173640f4af15a112686322d922a9021334
David Thomas <dev@synth3.tk>
2020-07-01 11:27:09 -0400

    update url env default

--- app/config/server.js
+++ app/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', `http://${host}:${port}`)
+  url: env('URL', `http://${strapi.config.server.host}:${strapi.config.server.port}`)
 });
